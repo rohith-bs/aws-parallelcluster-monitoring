b9b8dfafc601d47717a1672a12ab5a57ff10d0e2
Rohith B S <77735942+rohith-bs@users.noreply.github.com>
2022-09-03 14:01:02 +0530

    Update computefleet.sh

--- bootstraps/computefleet.sh
+++ bootstraps/computefleet.sh
@@ -1,1 +1,4 @@
 echo "bind path = /analysis" | tee -a /etc/singularity/singularity.conf
+mkdir -p /work/jobstore/
+mkdir -p /job_stores/staged_uploads
+ln -s /job_stores/staged_uploads /work/jobstore/
